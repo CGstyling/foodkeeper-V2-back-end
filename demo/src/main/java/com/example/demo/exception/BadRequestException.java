ec08a1779c6f7ae1eedf2221833414ed6c07ff00
CGstyling <christinaguseva@gmail.com>
2021-11-26 11:09:23 +0100

    exceptions

--- demo/src/main/java/com/example/demo/exception/BadRequestException.java
+++ demo/src/main/java/com/example/demo/exception/BadRequestException.java
@@ -1,4 +1,8 @@
 package com.example.demo.exception;
 
-public class BadRequestException {
+public class BadRequestException  extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public BadRequestException() {
+        super("Bad request..");
+    }
 }
---
+++ demo/src/main/java/com/example/demo/exception/InvalidPasswordException.java
@@ -0,0 +1,12 @@
+package com.example.demo.exception;
+
+public class InvalidPasswordException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+    public InvalidPasswordException() {
+        super("Invalid password.");
+    }
+}
