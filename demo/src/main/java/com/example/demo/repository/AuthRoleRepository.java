bfe22cee7a93be399e7e64f4f70fe87daf5e5135
CGstyling <christinaguseva@gmail.com>
2021-11-27 12:05:29 +0100

    SignIn method made in AuthRoleService

--- demo/src/main/java/com/example/demo/repository/AuthRoleRepository.java
+++ demo/src/main/java/com/example/demo/repository/AuthRoleRepository.java
@@ -1,11 +1,17 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.AuthRole;
+import com.example.demo.model.EAuthRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AuthRoleRepository extends JpaRepository<AuthRole, Long> {
 
-//    Optional<AuthorityRole> findByName(EAuthorityRole name); //make methods in auth controller/service
+    static Optional<AuthRole> findByName(EAuthRole name) //make methods in auth controller/service
+    {
+        return null;
+    }
 }
--- demo/src/main/java/com/example/demo/service/AuthRoleService.java
+++ demo/src/main/java/com/example/demo/service/AuthRoleService.java
@@ -1,12 +1,22 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.request.UserLoginRequest;
 import com.example.demo.dto.request.UserSignUpRequest;
 import com.example.demo.exception.RecordNotFoundException;
+import com.example.demo.exception.UserNotFoundException;
+import com.example.demo.model.AuthRole;
+import com.example.demo.model.EAuthRole;
 import com.example.demo.model.User;
 import com.example.demo.repository.AuthRoleRepository;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class AuthRoleService {
@@ -14,12 +24,20 @@ public class AuthRoleService {
     @Autowired
     AuthRoleRepository authRoleRepository;
     UserRepository userRepository;
-    public AuthRoleService(AuthRoleRepository authRoleRepository, UserRepository userRepository) {
+    JwtUtil jwtUtil;
+    PasswordEncoder passwordEncoder;
+    AuthenticationManager authenticationManager;
+    public AuthRoleService(AuthRoleRepository authRoleRepository, UserRepository userRepository, JwtUtil jwtUtil, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.authRoleRepository = authRoleRepository;
         this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
-    public void registerUser(UserSignUpRequest userSignUpRequest) {
+    public void signUpUser(UserSignUpRequest userSignUpRequest) {
+
+        //check if user exist
         if(userRepository.existsByUsername(userSignUpRequest.getUsername())) {
             throw new RecordNotFoundException();
         }
@@ -28,9 +46,34 @@ public class AuthRoleService {
             throw new RecordNotFoundException();
         }
 
-        User user = new User(userSignUpRequest.getUsername(), getEmail(), encoder.encode(userSignUpRequest.getPassword()));
+        //Create new user and give role
+        User user = new User(userSignUpRequest.getUsername(),
+                userSignUpRequest.getEmail(),
+                passwordEncoder.encode(userSignUpRequest.getPassword()));
+
+        Set<String> setRoles = userSignUpRequest.getAuthRole();
+        Set<AuthRole> authRoles = new HashSet<>();
+
+        if(setRoles== null) {
+            AuthRole userRole = AuthRoleRepository.findByName(EAuthRole.ROLE_USER)
+                    .orElseThrow(() -> new UserNotFoundException("Error: User role is not found."));
+            authRoles.add(userRole);
+        } else setRoles.forEach(role -> {
+            switch (role) {
+                case "admin":
+                    AuthRole adminRole = AuthRoleRepository.findByName(EAuthRole.ROLE_ADMIN)
+                            .orElseThrow(() -> new UserNotFoundException("Error: User role is not found."));
+                    authRoles.add(adminRole);
+                    break;
+                default:
+                    AuthRole userRole = AuthRoleRepository.findByName(EAuthRole.ROLE_USER)
+                            .orElseThrow(() -> new UserNotFoundException("Error: User role is not found."));
+                    authRoles.add(userRole);
+            }
+        });
+
+        user.setAuthRoles(authRoles);
+        userRepository.save(user);
     }
-
-
 
 }
