5a40e9a299615c62322138623a7d402c71bc6900
CGstyling <christinaguseva@gmail.com>
2021-12-02 14:36:04 +0100

    implemented the file upload

---
+++ demo/src/main/java/com/example/demo/service/DBFileStorageService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.model.DBFile;
+import com.example.demo.repository.DBFileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+
+@Service
+public class DBFileStorageService {
+
+    @Autowired
+    private DBFileRepository dbFileRepository;
+    public DBFileStorageService(DBFileRepository dbFileRepository) {
+        this.dbFileRepository = dbFileRepository;
+    }
+
+    //upload image and store in DB
+    public DBFile storeFile(MultipartFile file) {
+
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        try {
+            if(fileName.contains("..")) {
+                throw new RuntimeException("Sorry! Filename contains invalid path sequence " + fileName);
+            }
+            DBFile dbFile = new DBFile(fileName, file.getContentType(), file.getBytes());
+            return dbFileRepository.save(dbFile);
+        } catch (IOException exception) {
+            throw new RuntimeException("Could not store file " + fileName + ". Please try again!", exception);
+        }
+    }
+
+    //Get image by ID (Download)
+    public DBFile getFile(String fileId) {
+        return dbFileRepository.findById(fileId)
+                .orElseThrow(() -> new RuntimeException("File with id: " + fileId + " is not found."));
+    }
+
+}
